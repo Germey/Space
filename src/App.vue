ebdc238fc8e7c9310e93eddb5b4da0ef7c9fac05
Germey <cqc@cuiqingcai.com>
2019-05-07 11:01:28 +0800

    update build

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main></Main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  import Main from './components/Main'
+
+  export default {
+    name: 'app',
+    components: {
+      Main
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    padding: 0;
+    margin: 0;
+  }
+  #app {
+    padding: 0;
+    margin: 0;
+  }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="hello">
+    <el-row>
+      <el-col>
+        <div class="bar">
+          <i class="icon el-icon-coffee-cup"></i>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row :gutter="15" class="inputs">
+      <el-col :span="12">
+        <el-input
+            class="input"
+            type="textarea"
+            placeholder="请输入内容"
+            v-model="input">
+        </el-input>
+      </el-col>
+      <el-col :span="12">
+        <el-input
+            class="input"
+            type="textarea"
+            v-model="output">
+        </el-input>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  const pangu = require('pangu');
+
+  export default {
+    name: 'Main',
+    data() {
+      return {
+        title: '空格处理',
+        input: null,
+        output: null,
+      }
+    },
+    watch: {
+      input() {
+        this.output = pangu.spacing(this.input)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  $bar-height: 40px;
+  $inputs-padding: 40px;
+  .bar {
+    width: 100%;
+    height: $bar-height;
+    background: #218ADC;
+    color: white;
+    .icon {
+      margin: 10px;
+    }
+  }
+
+  .inputs {
+    padding: $inputs-padding;
+    .input {
+      height: 100vh;
+      textarea {
+        height: 100vh;
+      }
+    }
+  }
+
+</style>
